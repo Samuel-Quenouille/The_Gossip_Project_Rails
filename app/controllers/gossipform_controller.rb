5a994869a3ffcbd7679be54b3e29405c95ae031c
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-05-10 22:38:01 +0200

    modifs 3eme jour

--- app/controllers/gossipform_controller.rb
+++ app/controllers/gossipform_controller.rb
@@ -1,45 +1,20 @@
 class GossipformController < ApplicationController
-    def form
-    
-    end
-    
-    def index
-        @gossips = Gossip.all# Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
-    end
-    
-    def show
-        @gossip = Gossip.find(params[:id])# Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
-    end
-    
-    def new
-        # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
-    end
-    
-    def create
-        @gossipform = Gossip.new(gossip_params) # avec xxx qui sont les données obtenues à partir du formulaire
 
+    def create
+        @gossip = Gossip.new('user_id' => User.all.sample.id,
+                            'title' => params[:title],
+                            'content' => params[:content]) # avec xxx qui sont les données obtenues à partir du formulaire
         if @gossip.save # essaie de sauvegarder en base @gossip
-            redirect_to @gossip# si ça marche, il redirige vers la page d'index du site
+            redirect_to root_path(success: true)# si ça marche, il redirige vers la page d'index du site
         else
-            render 'new'# sinon, il render la view new (qui est celle sur laquelle on est déjà)
+            render :new # sinon, il render la view new (qui est celle sur laquelle on est déjà)
         end
         # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
         # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
         # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
     end
     
-    def edit
-        # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
-    end
-    
-    def update
-        # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
-        # pour info, le contenu de ce formulaire sera accessible dans le hash params
-        # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
-    end
-    
-    def destroy
-        # Méthode qui récupère le potin concerné et le détruit en base
-        # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
+    def index
+        @gossips = Gossip.all # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'contact/contact'
-  get '/contact', to: 'contact#contact'
-  get 'team/team'
+  root to: redirect('/gossipform')
+  resources :gossipform
+  
   get '/team', to: 'team#team'
-  get 'static_pages/home'
-  root 'static_pages#home'
-  get '/home', to: 'static_pages#home'
-  get '/form', to: 'gossipform#form'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/contact', to: 'contact#contact'
 end
